b05cb4fb1252cc30a08645a1e5e0119170af2793
KHH <una2000@nate.com>
2022-05-06 08:28:25 +0900

    회원가입 라우팅

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -7,30 +7,31 @@ const conpw = document.querySelector('#conpw');
 const signUpBtn = document.querySelector('#button');
 
 signUpBtn.addEventListener('click', register);
-function register(){
+function register() {
+  if (!id.value) return alert('아이디를 입력해 주세요.');
+  if (pw.value !== conpw.value) return alert('비밀번호 확인이 일치하지 않습니다.');
+  
   const req = {
     id: id.value,
     iname: iname.value,
     pw: pw.value,
-    conpw: conpw.value
   };
-  console.log(req);
-  // fetch('/register', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(req),
-  // })
-  // .then((res) => res.json())
-  // .then((res) => {
-  //   if(res.success) {
-  //     location.href = '/'
-  //   } else {
-  //     alert(res.msg);
-  //   }
-  // })
-  // .catch((err) => {
-  //   console.error('등록 중 에러 발생');
-  // })
+  fetch('/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+  .then((res) => res.json())
+  .then((res) => {
+    if(res.success) {
+      location.href = '/login'
+    } else {
+      alert(res.msg);
+    }
+  })
+  .catch((err) => {
+    console.error('등록 중 에러 발생');
+  })
 };
