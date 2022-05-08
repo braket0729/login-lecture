a3401b861256b956dda07c8099c7a2d1558c1be1
KHH <una2000@nate.com>
2022-05-08 16:26:48 +0900

    환경변수

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const home = require('./src/routes/home');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+
+const home = require('./src/routes/home');
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
-// add.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', home);
 
 module.exports = app;
--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,13 +7,18 @@ class User {
   }
   async login(){
     const client = this.body;
-    const {id, pw} = await UserStorage.getUserInfo(client.id);
-    if (id) {
-      if (id === client.id && pw === client.pw) {
-        return{success: true};
-      }      return{success: false, msg: '비밀번호가 틀렸습니다.'}
+    try {
+      const {id, password} = await UserStorage.getUserInfo(client.id);
+      if (id) {
+        if (id === client.id && password === client.pw) {
+          return{success: true};
+        }
+        return{success: false, msg: '비밀번호가 틀렸습니다.'}
+      }
+      return{success: false, msg: '존재하지 않는 아이디 입니다.'}
+    } catch (err) {
+      return {success: false, msg: err};
     }
-    return{success: false, msg: '존재하지 않는 아이디 입니다.'}
   };
   async register() {
     const client = this.body;
